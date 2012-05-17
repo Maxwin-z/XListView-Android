b837e869e17cc659149ecb3fe3f08dc67ba62320
Maxwin-z <zwd2005@gmail.com>
2012-05-17 23:42:12 +0800

    change structure

---
+++ src/me/maxwin/view/XListViewHeader.java
@@ -0,0 +1,135 @@
+/**
+ * @file PullRefreshView.java
+ * @package com.tencent.weibu.view
+ * @create Apr 18, 2012 5:22:27 PM
+ * @author maxiezhang
+ * @description TODO
+ */
+package me.maxwin.view;
+
+import me.maxwin.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * @author maxiezhang
+ * 
+ */
+public class XListViewHeader extends LinearLayout {
+//	private final static String TAG = "PullRefreshView";
+	private LinearLayout mContainer;
+	private ImageView mArrowImageView;
+	private ProgressBar mProgressBar;
+	private TextView mHintTextView;
+	private int mState = STATE_NORMAL;
+
+	private Animation mRotateUpAnim;
+	private Animation mRotateDownAnim;
+	
+	private final int ROTATE_ANIM_DURATION = 180;
+	
+	public final static int STATE_NORMAL = 0;
+	public final static int STATE_READY = 1;
+	public final static int STATE_REFRESHING = 2;
+
+	public XListViewHeader(Context context) {
+		super(context);
+		initView(context);
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public XListViewHeader(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView(context);
+	}
+
+	private void initView(Context context) {
+		// 初始情况，设置下拉刷新view高度为0
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+				LayoutParams.FILL_PARENT, 0);
+		mContainer = (LinearLayout) LayoutInflater.from(context).inflate(
+				R.layout.xlistview_header, null);
+		addView(mContainer, lp);
+		setGravity(Gravity.BOTTOM);
+
+		mArrowImageView = (ImageView)findViewById(R.id.xlistview_header_arrow);
+		mHintTextView = (TextView)findViewById(R.id.xlistview_header_hint_textview);
+		mProgressBar = (ProgressBar)findViewById(R.id.xlistview_header_progressbar);
+		
+		mRotateUpAnim = new RotateAnimation(0.0f, -180.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateUpAnim.setFillAfter(true);
+		mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateDownAnim.setFillAfter(true);
+	}
+
+	public void setState(int state) {
+		if (state == mState) return ;
+		
+		if (state == STATE_REFRESHING) {	// 显示进度
+			mArrowImageView.clearAnimation();
+			mArrowImageView.setVisibility(View.INVISIBLE);
+			mProgressBar.setVisibility(View.VISIBLE);
+		} else {	// 显示箭头图片
+			mArrowImageView.setVisibility(View.VISIBLE);
+			mProgressBar.setVisibility(View.INVISIBLE);
+		}
+		
+		switch(state){
+		case STATE_NORMAL:
+			if (mState == STATE_READY) {
+				mArrowImageView.startAnimation(mRotateDownAnim);
+			}
+			if (mState == STATE_REFRESHING) {
+				mArrowImageView.clearAnimation();
+			}
+			mHintTextView.setText("下拉载入更多");
+			break;
+		case STATE_READY:
+			if (mState != STATE_READY) {
+				mArrowImageView.clearAnimation();
+				mArrowImageView.startAnimation(mRotateUpAnim);
+				mHintTextView.setText("松开刷新");
+			}
+			break;
+		case STATE_REFRESHING:
+			mHintTextView.setText("正在载入...");
+			break;
+			default:
+		}
+		
+		mState = state;
+	}
+	
+	public void setVisiableHeight(int height) {
+		// Qzlog.d(TAG, "header height:" + height);
+		if (height < 0)
+			height = 0;
+		LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContainer
+				.getLayoutParams();
+		lp.height = height;
+		mContainer.setLayoutParams(lp);
+	}
+
+	public int getVisiableHeight() {
+		return mContainer.getHeight();
+	}
+
+}
